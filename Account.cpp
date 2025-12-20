3d4a99afafe5a4f9c17560fe390ca5b15a8243bc
Momen-Shalaby <momenshalaby911@gmail.com>
2025-12-21 00:25:55 +0200

    Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -14,7 +14,6 @@ int Account::totalAccounts = 0;
 Account::Account()
 
 {
-    cout<<"Welcome to our Bank :)\n\n";
     accountID = 0;
 
     balance = 0.0;
@@ -38,67 +37,60 @@ Account::Account(int id, double bal)
 }
 
 void Account::choice(){
-       char s;
-    do {
-
-
     string answer,answer2,nID,name;
-	int id;
-	double bal;
-    cout<<"Your account is personal or business. (p/b)?\n"<<endl;
+    int id;
+    double bal;
+    cout<<"Your account is personal or business. (p/b)?\n";
     cin>>answer;
+
     if (answer=="p"|| answer=="P"){
         cout<<"Enter your id and balance and national ID respectively.\n";
         cin>>id>>bal>>nID;
         PersonalAccount pA(id,bal,nID);
-    	cout<<"Do you want to deposit or withdraw ? (d/w)\n"<<endl;
-    	cin>>answer2;
-    	if(answer2=="d" || answer2=="D"){
-    		cout<<"Enter deposit amount.\n"<<endl;
+        cout<<"Do you want to deposit or withdraw ? (d/w)\n";
+        cin>>answer2;
+        if(answer2=="d" || answer2=="D"){
+            cout<<"Enter deposit amount.\n";
             double amount;cin>>amount;
-    		pA.deposit(amount);
-    		pA.displayInfo();
-    		cout<<"Accounts created= "<<pA.getTotalAccounts();
-		}
-		else if (answer2=="w" || answer2=="W"){
-            cout<<"Enter withdraw amount.\n"<<endl;
+            pA.deposit(amount);
+            pA.displayInfo();
+            cout<<"Accounts created= "<<pA.getTotalAccounts();
+        }
+        else if (answer2=="w" || answer2=="W"){
+            cout<<"Enter withdraw amount.\n";
             double amount;cin>>amount;
-    		pA.withdraw(amount);
-
-    		pA.displayInfo();
-    		cout<<"Accounts created= "<<pA.getTotalAccounts();
-		}
-	}
-	else if (answer=="b"|| answer=="B"){
+            pA.withdraw(amount);
+            pA.displayInfo();
+            cout<<"Accounts created= "<<pA.getTotalAccounts();
+        }
+        this->balance = pA.balance;
+        this->accountID = id;
+    }
+    else if (answer=="b"|| answer=="B"){
         cout<<"Enter your id and balance and company name respectively.\n";
         cin>>id>>bal>>name;
         BusinessAccount bA(id,bal,name);
-    	cout<<"Do you want to deposit or withdraw ? (d/w)"<<endl;
-    	cin>>answer2;
-    	if(answer2=="d" || answer2=="D"){
-    		cout<<"Enter deposit amount.\n"<<endl;
+        cout<<"Do you want to deposit or withdraw ? (d/w)\n";
+        cin>>answer2;
+        if(answer2=="d" || answer2=="D"){
+            cout<<"Enter deposit amount.\n"<<endl;
             double amount;cin>>amount;
-    		bA.deposit(amount);
-
-    		bA.displayInfo();
-    		cout<<"Accounts created= "<<bA.getTotalAccounts()<<endl;
-		}
-		else if (answer2=="w" || answer2=="W"){
-            cout<<"Enter withdraw amount.\n"<<endl;
+            bA.deposit(amount);
+            bA.displayInfo();
+            cout<<"Accounts created= "<<bA.getTotalAccounts();
+        }
+        else if (answer2=="w" || answer2=="W"){
+            cout<<"Enter withdraw amount.\n";
             double amount;cin>>amount;
-    		bA.withdraw(amount);
-
-    		bA.displayInfo();
-    		cout<<"Accounts created= "<<bA.getTotalAccounts()<<endl;
-
-		}
-	}
-	cout << "\nIf you want to Continue : Enter C\n";
-    cin >> s ;
+            bA.withdraw(amount);
+            bA.displayInfo();
+            cout<<"Accounts created= "<<bA.getTotalAccounts();
+        }
+        this->balance = bA.balance;
+        this->accountID = id;
+    }
 }
- while (s=='c'||s=='C');
 
-}
 double Account::deposit(double amount)
 
 {
@@ -133,19 +125,17 @@ void Account::displayInfo()
 
 
 
-Account Account::operator+(const Account& result)
+double Account::operator+(const Account& obj)
 
 {
 
-    Account temp;
+    double result;
 
-    temp.accountID = this->accountID;
+    result = this->balance + obj.balance;
 
-    temp.balance = this->balance + result.balance;
 
-    totalAccounts--;
 
-    return temp;
+    return result;
 
 }
 
