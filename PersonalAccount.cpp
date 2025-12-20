29643bb923f9892ee0cb36aa2a81d77c93982d54
MarawanOmar <marawanomar56@gmail.com>
2025-12-20 23:51:04 +0200

    Update PersonalAccount.cpp

--- PersonalAccount.cpp
+++ PersonalAccount.cpp
@@ -1,21 +1,17 @@
-
 #include "PersonalAccount.h"
 #include <iostream>
 using namespace std;
-
 PersonalAccount::PersonalAccount(int id, double bal, string nID)
     : Account(id, bal)
 {
-    nationalID = nID;
+    nationalID = nID;
 }
-
 double PersonalAccount::withdraw(double amount)
 {
     if(amount <= 0){
         cout << "ERROR : Invalid amount" << endl;
         return 0.0;
     }
-
     if(amount > 5000){
         cout << "ERROR : Maximum withdrawal limit is 5000" << endl;
         return 0.0;
@@ -24,12 +20,10 @@ double PersonalAccount::withdraw(double amount)
         cout << "ERROR : Insufficient balance" << endl;
         return 0.0;
     }
-
     balance -= amount;
     return balance;
 }
-
 PersonalAccount::~PersonalAccount()
 {
-
+    cout << "\nPersonalAccount destroyed \n";
 }
