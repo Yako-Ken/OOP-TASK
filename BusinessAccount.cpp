332f73c70d2809f549cfd65413b5f304fe16710e
Yako-Ken <yako0ae@gmail.com>
2025-12-20 17:20:04 +0200

    Update BusinessAccount.cpp

--- BusinessAccount.cpp
+++ BusinessAccount.cpp
@@ -9,7 +9,22 @@ BusinessAccount::BusinessAccount(int id, double bal, string name)
 
 double BusinessAccount::withdraw(double amount)
 {
-    return 0.0;
+   if(amount <= 0){
+        cout << "ERROR : Invalid amount" << endl;
+        return 0.0;
+    }
+
+    if(amount > 50000){
+        cout << "ERROR : Maximum withdrawal limit is 5000" << endl;
+        return 0.0;
+    }
+    if(amount > balance){
+        cout << "ERROR : Insufficient balance" << endl;
+        return 0.0;
+    }
+
+    balance -= amount;
+    return balance;
 }
 
 BusinessAccount::~BusinessAccount()
