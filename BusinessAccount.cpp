ad5b96a1b1b2d0ece5da772455d4bf80ae3fc1cd
Momen-Shalaby <momenshalaby911@gmail.com>
2025-12-21 00:27:03 +0200

    BusinessAccount.cpp

--- BusinessAccount.cpp
+++ BusinessAccount.cpp
@@ -1,29 +1,70 @@
 #include "BusinessAccount.h"
+
 #include <iostream>
+
 using namespace std;
+
+
+
 BusinessAccount::BusinessAccount(int id, double bal, string name)
+
     : Account(id, bal)
+
 {
+
     businessName = name;
+
 }
+
+
+
 double BusinessAccount::withdraw(double amount)
+
 {
+
    if(amount <= 0){
+
         cout << "ERROR : Invalid amount" << endl;
+
         return 0.0;
+
     }
-    if(amount > 50000){
+
+
+
+    else if(amount > 50000){
+
         cout << "ERROR : Maximum withdrawal limit is 50000" << endl;
+
         return 0.0;
+
     }
-    if(amount > balance){
+
+    else if(amount > balance){
+
         cout << "ERROR : Insufficient balance" << endl;
+
         return 0.0;
+
     }
+
+
+    else{
+
     balance -= amount;
+
     return balance;
+    }
+
 }
+
+
+
 BusinessAccount::~BusinessAccount()
+
 {
-    cout << "BusinessAccount destroyed \n";
+
+    cout<<"\n\n------------------------------ Business Account Object Destroyed! ------------------------------\n\n";
+
+
 }
