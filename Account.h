31140b0cda33cda45e23680dc08b61ca22af8e4a
Momen-Shalaby <momenshalaby911@gmail.com>
2025-12-20 21:55:59 +0200

    Account.h

--- Account.h
+++ Account.h
@@ -1,20 +1,40 @@
 #ifndef ACCOUNT_H
+
 #define ACCOUNT_H
 
+
+
 class Account
+
 {
+
 protected:
+
     int accountID;
+
     double balance;
+
     static int totalAccounts;
 
+
 public:
+
     Account();
+
     Account(int id, double bal);
+
+    void choice();
+
     double deposit(double amount);
+
     virtual void displayInfo();
+
     static int getTotalAccounts();
+
     Account operator+(const Account& result);
+
 };
+
+
 
 #endif
