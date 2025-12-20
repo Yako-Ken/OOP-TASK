8353c5219495fc190832c9512dcd13874fb16ccf
MarawanOmar <marawanomar56@gmail.com>
2025-12-20 19:08:16 +0200

    Update PersonalAccount.cpp

--- PersonalAccount.cpp
+++ PersonalAccount.cpp
@@ -6,7 +6,7 @@ using namespace std;
 PersonalAccount::PersonalAccount(int id, double bal, string nID)
     : Account(id, bal)
 {
-    PersonalName = name;
+    NationalID = nID;
 }
 
 double PersonalAccount::withdraw(double amount)
