d59c5ab5c40ce796e5890071ec4bb0c44480a515
youssefgamalmowafy-spec <youssefgamalmowafy@gmail.com>
2025-12-20 12:24:17 +0200

    Enhance Account class with constructors and methods
    Initialize accountID and balance in constructors, update deposit method to return new balance, and implement displayInfo method.

--- Account.cpp
+++ Account.cpp
@@ -6,28 +6,44 @@ int Account::totalAccounts = 0;
 
 Account::Account()
 {
+    accountID = 0;
+    balance = 0.0;
+    totalAccounts++;
 }
 
 Account::Account(int id, double bal)
 {
+    accountID = id;
+    balance = bal;
+    totalAccounts++;
 }
 
 double Account::deposit(double amount)
 {
-    return 0.0;
+    if(amount > 0)
+    {
+        balance += amount;
+    }
+    return balance;
 }
 
 void Account::displayInfo()
 {
-
+    cout << "Account ID: " << accountID << endl;
+    cout << "Balance: " << balance << endl;
 }
 
 Account Account::operator+(const Account& result)
 {
-    
+    Account temp;
+    temp.accountID = this->accountID;
+    temp.balance = this->balance + result.balance;
+    totalAccounts--;
+    return temp;
 }
 
 int Account::getTotalAccounts()
 {
+    return totalAccounts;
 }
 
